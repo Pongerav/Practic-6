3b375f919e3b463c001df8d6cf16ffbb48a55121
Pavel Ast a.k.a Pong <pavel.astashkin.05@mail.ru>
2022-09-11 09:27:31 +0300

    Added first exercise

---
+++ 001_MxN_Float/Program.cs
@@ -0,0 +1,25 @@
+﻿void FillArrayFloat(double[,] array)
+{
+    for (int i = 0; i <= array.GetUpperBound(0); i++)
+    {
+        for (int j = 0; j <= array.GetUpperBound(1); j += 1) array[i,j] = new Random().NextDouble() + new Random().Next(0,1001);
+    }
+}
+void PrintArray(double[,] collection)
+{
+    for (int i = 0; i <= collection.GetUpperBound(0); i++)
+    {
+        Console.WriteLine();
+        for (int j = 0; j <= collection.GetUpperBound(1); j += 1) Console.Write(collection[i,j].ToString("#.##") + "   ");
+    }
+}
+
+Console.WriteLine("Введите количество строчек массива");
+int m = int.Parse(Console.ReadLine() ?? "0");
+Console.WriteLine("Введите количество столбцов массива");
+int n = int.Parse(Console.ReadLine() ?? "0");
+
+double[,] massiv = new double[m,n];
+FillArrayFloat(massiv);
+PrintArray(massiv);
+
