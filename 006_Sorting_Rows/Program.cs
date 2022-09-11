ac9510c8c9107282126193a3aa7ba07acfefe4c4
Pavel Ast a.k.a Pong <pavel.astashkin.05@mail.ru>
2022-09-11 12:59:49 +0300

    Added sorting of array

---
+++ 006_Sorting_Rows/Program.cs
@@ -0,0 +1,47 @@
+﻿void FillArray(int[,] array)
+{
+    for (int i = 0; i <= array.GetUpperBound(0); i++)
+    {
+        for (int j = 0; j <= array.GetUpperBound(1); j += 1) array[i,j] = new Random().Next(0,21);
+}
+}
+
+void PrintArray(int[,] collection)
+{
+    for (int i = 0; i <= collection.GetUpperBound(0); i++)
+    {
+        Console.WriteLine();
+        for (int j = 0; j <= collection.GetUpperBound(1); j += 1) Console.Write(collection[i,j] + "    ");
+    }
+}
+void Sorting(int[,] array,int i)
+{
+    int x = 0;
+    for (int l = 1; l < array.GetUpperBound(1); l++)
+    {
+        for (int j = 0; j <= array.GetUpperBound(1)-l; j++)
+        {
+            if (array[i,j] > array[i,j+1])
+            {
+                x = array[i,j];
+                array[i,j] = array[i,j+1];
+                array[i,j+1] = x;
+            }
+        }
+    }
+}
+
+Console.WriteLine("Введите количество строчек массива");
+int m = int.Parse(Console.ReadLine() ?? "0");
+Console.WriteLine("Введите количество столбцов массива");
+int n = int.Parse(Console.ReadLine() ?? "0");
+
+int[,] massiv = new int[m,n];
+FillArray(massiv);
+PrintArray(massiv);
+Console.WriteLine();
+for (int i = 0; i <= massiv.GetUpperBound(0); i += 1)
+{
+    Sorting(massiv,i);
+}
+PrintArray(massiv);
